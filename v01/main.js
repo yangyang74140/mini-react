c86d085685e43be48596566235396d23722d51c4
yangshunqin <1437596648@qq.com>
2025-03-02 08:19:13 +0800

    feat: 页面中显示app

--- v01/main.js
+++ v01/main.js
@@ -1,2 +1,87 @@
-console.log("Hello World");
-console.log("创建新分支");
+// v1
+// const dom = document.createElement("div");
+// dom.id = "app";
+
+// const text = document.createTextNode("");
+// text.nodeValue = "app";
+// dom.append(text);
+
+// document.querySelector("#root").append(dom);
+
+// v2
+
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "app",
+//     children: [],
+//   },
+// };
+
+// const el = {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [textEl],
+//   },
+// };
+
+// const dom = document.createElement(el.type);
+
+// dom.id = el.props.id;
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
+
+// dom.append(textNode);
+
+// document.querySelector("#root").append(dom);
+
+// v3
+
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "app",
+//     children: [],
+//   },
+// };
+
+const createTextNode = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+};
+
+// const el = {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [textEl],
+//   },
+// };
+
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: { ...props, children },
+  };
+};
+
+const textEl = createTextNode("app");
+
+const App = createElement("div", { id: "app" }, textEl);
+
+const dom = document.createElement(App.type);
+dom.id = App.props.id;
+
+const textNode = document.createTextNode("");
+textNode.nodeValue = textEl.props.nodeValue;
+
+dom.append(textNode);
+
+document.querySelector("#root").append(dom);
