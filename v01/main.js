ce9980856120a5e6f9a135bb15650cd3395b8e9e
yangshunqin <1437596648@qq.com>
2025-03-26 08:15:27 +0800

    动态创建

--- v01/main.js
+++ v01/main.js
@@ -47,6 +47,47 @@
 //   },
 // };
 
+// const createTextNode = (text) => {
+//   return {
+//     type: "TEXT_ELEMENT",
+//     props: {
+//       nodeValue: text,
+//       children: [],
+//     },
+//   };
+// };
+
+// const el = {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [textEl],
+//   },
+// };
+
+// const createElement = (type, props, ...children) => {
+//   return {
+//     type,
+//     props: { ...props, children },
+//   };
+// };
+
+// const textEl = createTextNode("app");
+
+// const App = createElement("div", { id: "app" }, textEl);
+
+// const dom = document.createElement(App.type);
+// dom.id = App.props.id;
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
+
+// dom.append(textNode);
+
+// document.querySelector("#root").append(dom);
+
+// v4
+
 const createTextNode = (text) => {
   return {
     type: "TEXT_ELEMENT",
@@ -57,31 +98,56 @@ const createTextNode = (text) => {
   };
 };
 
-// const el = {
-//   type: "div",
-//   props: {
-//     id: "app",
-//     children: [textEl],
-//   },
-// };
-
 const createElement = (type, props, ...children) => {
   return {
     type,
-    props: { ...props, children },
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child === "string" ? createTextNode(child) : child;
+      }),
+    },
   };
 };
 
+function render(el, container) {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(App.type);
+
+  // props
+  Object.keys(el.props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = el.props[key];
+    }
+  });
+
+  // children
+  const children = el.props.children;
+  children.forEach((child) => {
+    render(child, dom);
+  });
+  console.log("dom", dom);
+  console.log("container", container);
+
+  container.append(dom);
+}
+
 const textEl = createTextNode("app");
 
-const App = createElement("div", { id: "app" }, textEl);
+const App = createElement("div", { id: "app" }, "hi-", textEl);
 
-const dom = document.createElement(App.type);
-dom.id = App.props.id;
+// render(App, document.querySelector("#root"));
 
-const textNode = document.createTextNode("");
-textNode.nodeValue = textEl.props.nodeValue;
+const ReactDOM = {
+  createRoot(container) {
+    return {
+      render(App) {
+        render(App, container);
+      },
+    };
+  },
+};
 
-dom.append(textNode);
-
-document.querySelector("#root").append(dom);
+ReactDOM.createRoot(document.querySelector("#root")).render(App);
