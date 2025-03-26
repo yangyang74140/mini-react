59d486b09d7844168c8fff84d428d72ea9e8ce21
yangshunqin <1437596648@qq.com>
2025-03-27 07:46:51 +0800

    feat:使用vite实现jsx

---
+++ v01.2-jsx/vite-runner/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "./core/React.js";
+// const App = React.createElement("div", { id: "app" }, "hi-", "mini-", "react");
+const App = <div>hi-mini-react-jsx</div>
+
+export default App;
---
+++ v01.2-jsx/vite-runner/src/core/React.js
@@ -0,0 +1,50 @@
+const createTextNode = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+};
+
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child === "string" ? createTextNode(child) : child;
+      }),
+    },
+  };
+};
+
+function render(el, container) {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(el.type);
+
+  // props
+  Object.keys(el.props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = el.props[key];
+    }
+  });
+
+  // children
+  const children = el.props.children;
+  children.forEach((child) => {
+    render(child, dom);
+  });
+
+  container.append(dom);
+}
+
+const React = {
+  render,
+  createElement,
+};
+
+export default React;
